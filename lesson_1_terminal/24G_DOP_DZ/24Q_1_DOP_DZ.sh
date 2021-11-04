01fa095edefbd80afdb5878c2bf93f0405ce30a6
Palad0s <irokez1sk@gmail.com>
2021-11-04 21:52:58 +0200

    First HM about Terminal

---
+++ lesson_1_terminal/24G_DOP_DZ/24Q_1_DOP_DZ.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+ # скрипт который выполнит автоматически пункты 3, 4, 5, 6, 7, 8, 13
+ mkdir dp;
+ cd dp;
+ mkdir less_1 less_2 less_3; 
+ cd less_3;
+ touch 11.txt 12.txt 13.txt 11.json 12.json; 
+ mkdir mur_1 mur_2 mur_3;
+ ls; 
+  mv 11.txt 11.json mur_2/.
